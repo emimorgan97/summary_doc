8c25e0d634e9a3026eda2ceebd1b75f2f44a4014
emimorgan97 <ealmorgan@gmail.com>
2024-11-28 15:58:10 -0500

    starting to write out what UI needs

--- main.py
+++ main.py
@@ -1,1 +1,5 @@
-import streamlit
+import streamlit as st
+
+st.set_page_config(layout = "wide")
+st.selectbox("Your document is what type of document?", ["journal article", "pamphlet", "short book", "book", "short story", "other"])
+st.file_uploader("Upload the document you want summarized", type = ['pdf'], accept_multiple_files = False)
